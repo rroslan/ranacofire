f9a2c8e30694af2cc8d07e6b79fe02fc573fefef
rrosl <rroslan@gmail.com>
2016-11-17 18:49:48 +0800

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+export const firebaseConfig = {
+  apiKey: "AIzaSyD3r907DxMWaxbqWTT-ltcdxaUQyA3CYgI",
+  authDomain: "ranaco.firebaseapp.com",
+  databaseURL: "https://ranaco.firebaseio.com",
+  storageBucket: "firebase-ranaco.appspot.com"
+};
 
 @NgModule({
   declarations: [
@@ -12,9 +18,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
